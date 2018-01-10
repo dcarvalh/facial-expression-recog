48bf0fda77ef771f73786264fcf337ccf3c31770
Woody <aym.collange@gmail.com>
2018-01-10 20:20:42 +0100

    moved functions handling data in a new file

--- classification.py
+++ classification.py
@@ -7,6 +7,8 @@ from keras.layers import Dense, GlobalAveragePooling2D
 from keras import backend as K
 import numpy as np
 
+from handle_data import generate_tuples
+
 number_classes = 7
 classes = 
 # creates our CNN
@@ -55,30 +57,6 @@ model.compile(optimizer='rmsprop', loss='categorical_crossentropy')
 # fit_generator(self, generator, steps_per_epoch=None, epochs=1, verbose=1, callbacks=None, validation_data=None, validation_steps=None, class_weight=None, max_queue_size=10, workers=1, use_multiprocessing=False, shuffle=True, initial_epoch=0)
 # TODO adapt generate_tuples to our case
 generator = generate_tuples('path/to/train')
-
-# Returns a numpy array based from an image file
-def process_line(img_path):
-	img = image.load_img(img_path, target_size=(224, 224))
-	x = image.img_to_array(img)
-	x = np.expand_dims(x, axis=0)
-	x = preprocess_input(x)
-	return x
-
-
-# Returns a generator
-def generate_tuples(path):
-	#while 1:
-	# Get the list of all classes (= list of folder in the dataset path)
-	classes = [d for d in os.listdir(path) is os.path.isdir(os.path.join(path, d))]
-	# For each class...
-	for emotion in classes:
-		path_e = os.path.join(path, emotion))
-		# For each image file in this directory...
-		for img_name in os.listdir(path_e):
-			# create Numpy arrays of input data
-			img = process_line(os.path.join(path_e, img_name))
-			# yield data and label
-			yield (data, emotion)
 
 batch_size = 30;
 # samples_per_epoch = size(training set)/batch_size
---
+++ handle_data.py
@@ -0,0 +1,25 @@
+import os
+import numpy as np
+# Returns a numpy array based from an image file
+def process_file(img_path):
+	img = image.load_img(img_path, target_size=(224, 224))
+	x = image.img_to_array(img)
+	x = np.expand_dims(x, axis=0)
+	x = preprocess_input(x)
+	return x
+
+
+# Returns a generator
+def generate_tuples(path):
+	#while 1:
+	# Get the list of all classes (= list of folder in the dataset path)
+	classes = [d for d in os.listdir(path) is os.path.isdir(os.path.join(path, d))]
+	# For each class...
+	for emotion in classes:
+		path_e = os.path.join(path, emotion)
+		# For each image file in this directory...
+		for img_name in os.listdir(path_e):
+			# create Numpy arrays of input data
+			img = process_line(os.path.join(path_e, img_name))
+			# yield data and label
+			yield (data, emotion)
