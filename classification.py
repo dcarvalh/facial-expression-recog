9654688adc490e1ee98a40ba2ecc41a98e51def9
Woody <aym.collange@gmail.com>
2018-01-22 14:47:39 +0100

    removed useless code at the end of the main file

--- classification.py
+++ classification.py
@@ -102,31 +102,8 @@ test_generator = test_datagen.flow_from_directory('./test',
 	target_size = (224, 224),
 	batch_size = 32)
 # Evaluates our model on the testing set and print accuracy and loss
-print(model.metrics_names);
-model.evaluate_generator(test_generator);
+print(model.metrics_names)
+model.evaluate_generator(test_generator)
 
+# TODO also call predict_generator to get the list of results
 
-
-
-
-
-
-
-
-
-############ Use the basic model without finetuning it 
-def use_base_model(number_classes=7):
-# creates our CNN
-	model = ResNet50(weights='imagenet', classes=number_classes)
-
-	img_path = 'elephant.jpg'
-	img = image.load_img(img_path, target_size=(224, 224))
-	x = image.img_to_array(img)
-	x = np.expand_dims(x, axis=0)
-	x = preprocess_input(x)
-
-	preds = model.predict(x)
-	# decode the results into a list of tuples (class, description, probability)
-	# (one such list for each sample in the batch)
-	print('Predicted:', decode_predictions(preds, top=3)[0])
-	# Predicted: [(u'n02504013', u'Indian_elephant', 0.82658225), (u'n01871265', u'tusker', 0.1122357), (u'n02504458', u'African_elephant', 0.061040461)]
