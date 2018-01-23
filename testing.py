8f5931bb8d2c99c6ce70badb6a7d8c92258f8111
Woody <aym.collange@gmail.com>
2018-01-23 22:08:24 +0100

    code style & comment for CMC

--- testing.py
+++ testing.py
@@ -1,8 +1,10 @@
 from Inception import Inception
 from DSHandler import DSHandler
 from keras.models import load_model
+import numpy as np
+import matplotlib.pyplot as plt
 
-number_classes = 7
+number_classes = 6
 
 # Model definition
 dsHandler = DSHandler((175, 175))
@@ -27,12 +29,10 @@ for v, k in classes_view:
 classifier.conf_matrix(test_gen)
 
 #### CMC
-# gives expected output for each sample
-expected = test_gen.classes
 # gives results for each sample as a list of probability
 results = classifier.model.predict_generator(test_gen)
-i = 0
 
+i = 0
 # Build a new array containing only ranks
 for r in results:
 	temp = r.argsort()
@@ -40,25 +40,32 @@ for r in results:
 	ranks = np.empty_like(temp)
 	# Returns range of evenly spaced value between 0 and len(r)
 	ranks[temp] = np.arange(len(r))
+	# update our array with those ranks
 	results[i]=ranks
 	i = i+1
 
 i = 0
 cmc = [0, 0, 0, 0, 0, 0]
+
+# gives expected output for each sample
+expected = test_gen.classes
 # Get the rank of each element and sum it for our array
+# TODO find a simpler way to do it
 for e in expected:
 	rank = int(results[i][e])
 	cmc[rank] = cmc[rank] + 1	
 	i=i+1
 
-# Should be equal to the number of samples
-print(sum(cmc))
+
 # As the rank was inverse earlier, we need to reverse it again
 cmc = cmc[::-1]
+print(sum(cmc)) # Should be equal to the number of samples
+
 # change values of cmc to get only the probabilities
 cmc = [float(x) / 39. for x in cmc]
-# Should be (at least almost) equal to 1
-print(sum(cmc))
+
+print(sum(cmc)) # Should be (at least almost) equal to 1
+
 # Computes the cumulative sum of those probabilities
 cmc = [sum(cmc[:i]) for i in range(1, len(cmc)+1)]
 
