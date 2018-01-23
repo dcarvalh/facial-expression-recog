51e5bc45ccb93fcca0946e91e7a22cbff1414655
Woody <aym.collange@gmail.com>
2018-01-23 15:41:40 +0100

    found good parameters for data augmentation (go up to 35% for validation)

--- DSHandler.py
+++ DSHandler.py
@@ -8,15 +8,15 @@ class DSHandler:
 
 	# Set the data generator for the training dataset
 	# Also set modifiers for data augmentation (shear and zoom)
-	def get_train_generator(self, dataset):
-		# horizontal_flip = True
-		# zoom_range=0.05
-		# shear_range = 0.3,
+	def get_train_generator(self, image_directory):
+		# Creates generator for training set and applies data augmentation parameters
 		train_datagen = ImageDataGenerator(
-			rescale = self.rescale, 
-			horizontal_flip = True)
+			rescale = self.rescale,
+			horizontal_flip = True,
+			zoom_range=0.05,
+			shear_range = 0.3)
 		train_generator = train_datagen.flow_from_directory(
-			dataset,
+			image_directory,
 			target_size = self.resize,
 			batch_size = 32)
 
@@ -32,10 +32,12 @@ class DSHandler:
 #				break  # otherwise the generator would loop indefinitely
 		return train_generator
 
-	def get_generator(self, dataset):
+	# Set the data generator for validation and testing set
+	# Also set modifiers for data augmentation (shear and zoom)
+	def get_generator(self, image_directory):
 		datagen = ImageDataGenerator(rescale = self.rescale)
 		generator = datagen.flow_from_directory(
-			dataset,
+			image_directory,
 			target_size = self.resize,
 			batch_size = 32)
 		return generator
