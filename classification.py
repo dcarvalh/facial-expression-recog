69efed3c56f8fa5e437285d5b14ce4ca64a451fc
david-bcarvalho <dmbfc11@gmail.com>
2018-01-23 11:32:50 +0100

    save model image

--- classification.py
+++ classification.py
@@ -1,6 +1,7 @@
 
 from ResNet import ResNet
 from DSHandler import DSHandler
+from keras.utils import plot_model
 
 number_classes = 7
 
@@ -17,7 +18,17 @@ classifier.fine_tune(dsHandler.train_generator, dsHandler.validation_generator)
 print("Evaluates classifier on testing set...")
 classifier.evaluate(dsHandler.test_generator)
 
+my_model = classifier.model
 
 
+#plot_model(my_model, to_file='model.png')
+
+
+"""
+layers
+inputs
+outputs
+    
+"""
 
 
